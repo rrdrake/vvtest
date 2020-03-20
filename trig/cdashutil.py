c372ba28c0f3d8a944bae7d921c44175ecfa412a
Richard R. Drake <rrdrake@sandia.gov>
2020-03-19 22:04:15 -0600

    add test results creation class
    only writes global fields (like dates) in this commit

--- trig/cdashutil.py
+++ trig/cdashutil.py
@@ -9,6 +9,59 @@ sys.dont_write_bytecode = True
 sys.excepthook = sys.__excepthook__
 import os
 import subprocess
+import time
+
+
+class TestResultsFormatter:
+
+    def __init__(self):
+        ""
+        self.date = None
+        self.group = None
+        self.site = None
+        self.build = None
+        self.start = None
+        self.end = None
+
+    def setBuildID(self, build_date=None,
+                         build_group=None,
+                         site_name=None,
+                         build_name=None ):
+        ""
+        if build_date  != None: self.date  = int( build_date )
+        if build_group != None: self.group = build_group
+        if site_name   != None: self.site  = site_name
+        if build_name  != None: self.build = build_name
+
+    def setTime(self, start_time, end_time=None):
+        ""
+        self.start = start_time
+        if end_time != None: self.end = end_time
+
+    def writeToFile(self, filename):
+        ""
+        dt = self.date
+        if dt == None: dt = int( time.time() )
+
+        grp = self.group
+        if grp == None: grp = 'Experimental'
+
+        site = self.site
+        if site == None: site = os.uname()[1]
+
+        bld = self.build
+        if bld == None: bld = 'tests'
+
+        stamp = make_build_stamp( dt, grp )
+
+        with open( filename, 'wt' ) as fp:
+            write_xml_header( fp )
+            start_element( fp, 'Site', BuildStamp=stamp,
+                                       Name=site,
+                                       BuildName=bld,
+                                       Append='true' )
+            write_time_section( fp, dt, self.start, self.end )
+            end_element( fp, 'Site' )
 
 
 def submit_file( cdash_url, project_name, filename, method='urllib' ):
@@ -27,6 +80,57 @@ def submit_file( cdash_url, project_name, filename, method='urllib' ):
         else:
             cmd = 'curl -T '+filename+' '+submit_url
             subprocess.check_call( cmd, shell=True )
+
+
+def start_element( fp, name, **kwargs ):
+    ""
+    fp.write( '<'+name )
+    for n,v in kwargs.items():
+        fp.write( ' '+n+'="'+v+'"' )
+    fp.write( '>\n' )
+
+
+def end_element( fp, name ):
+    ""
+    fp.write( '</'+name+'>\n' )
+
+
+def write_time_section( fp, stamp_date, start, end ):
+    ""
+    if start == None: start = stamp_date
+    if end == None: end = start
+
+    elapsed = ( end-start ) / 60
+
+    start_element( fp, 'Testing' )
+    simple_element( fp, 'StartDateTime', string_date(start) )
+    simple_element( fp, 'StartTestTime', str(start) )
+    simple_element( fp, 'EndDateTime', string_date(end) )
+    simple_element( fp, 'EndTestTime', str(end) )
+    simple_element( fp, 'ElapsedMinutes', str(elapsed) )
+    end_element( fp, 'Testing' )
+
+
+def write_xml_header( fp ):
+    ""
+    fp.write( '<?xml version="1.0" encoding="UTF-8"?>\n' )
+
+
+def simple_element( fp, name, value ):
+    ""
+    fp.write( '<'+name+'>'+value+'</'+name+'>\n' )
+
+
+def make_build_stamp( epoch, group ):
+    ""
+    stamp = time.strftime( '%Y%m%d-%H%M%S-', time.localtime(epoch) )
+    stamp += group
+    return stamp
+
+
+def string_date( epoch ):
+    ""
+    return time.strftime( '%b %d %H:%M:%S %Z', time.localtime(epoch) )
 
 
 def submit_file_using_urllib( submit_url, filename ):
