f2bb6d87a3e6837ae99d9e98b8bed358869aea1e
Richard R. Drake <rrdrake@sandia.gov>
2020-03-20 21:19:07 -0600

    add the tests to the cdash file format

--- trig/cdashutil.py
+++ trig/cdashutil.py
@@ -22,6 +22,7 @@ class TestResultsFormatter:
         self.build = None
         self.start = None
         self.end = None
+        self.tests = []
 
     def setBuildID(self, build_date=None,
                          build_group=None,
@@ -38,6 +39,16 @@ class TestResultsFormatter:
         self.start = start_time
         if end_time != None: self.end = end_time
 
+    def addTest(self, name, prefix='.',
+                            status='passed',
+                            cmdline='',
+                            runtime=None,
+                            detail='',
+                            output='' ):
+        ""
+        tup = ( name, prefix, status, cmdline, runtime, detail, output )
+        self.tests.append( tup )
+
     def writeToFile(self, filename):
         ""
         dt   = int( time.time() ) if self.date  == None else self.date
@@ -48,12 +59,19 @@ class TestResultsFormatter:
         stamp = make_build_stamp( dt, grp )
 
         with open( filename, 'wt' ) as fp:
+
             write_xml_header( fp )
+
             start_element( fp, 'Site', BuildStamp=stamp,
                                        Name=site,
                                        BuildName=bld,
                                        Append='true' )
+
+            start_element( fp, 'Testing' )
             write_time_section( fp, dt, self.start, self.end )
+            write_tests_section( fp, self.tests )
+            end_element( fp, 'Testing' )
+
             end_element( fp, 'Site' )
 
 
@@ -75,19 +93,6 @@ def submit_file( cdash_url, project_name, filename, method='urllib' ):
             subprocess.check_call( cmd, shell=True )
 
 
-def start_element( fp, name, **kwargs ):
-    ""
-    fp.write( '<'+name )
-    for n,v in kwargs.items():
-        fp.write( ' '+n+'="'+v+'"' )
-    fp.write( '>\n' )
-
-
-def end_element( fp, name ):
-    ""
-    fp.write( '</'+name+'>\n' )
-
-
 def write_time_section( fp, stamp_date, start, end ):
     ""
     if start == None: start = stamp_date
@@ -95,13 +100,85 @@ def write_time_section( fp, stamp_date, start, end ):
 
     elapsed = ( end-start ) / 60
 
-    start_element( fp, 'Testing' )
     simple_element( fp, 'StartDateTime', string_date(start) )
     simple_element( fp, 'StartTestTime', str(start) )
     simple_element( fp, 'EndDateTime', string_date(end) )
     simple_element( fp, 'EndTestTime', str(end) )
     simple_element( fp, 'ElapsedMinutes', str(elapsed) )
-    end_element( fp, 'Testing' )
+
+
+def write_tests_section( fp, testlist ):
+    ""
+    start_element( fp, 'TestList' )
+
+    for name,prefix,status,cmdline,runtime,detail,output in testlist:
+        simple_element( fp, 'Test', prefix+'/'+name )
+
+    end_element( fp, 'TestList' )
+
+    for results in testlist:
+        write_test_result( fp, results )
+
+
+def write_test_result( fp, results ):
+    ""
+    name,prefix,status,cmdline,runtime,detail,output = results
+
+    start_element( fp, 'Test', Status=status )
+
+    simple_element( fp, 'Name', name )
+    simple_element( fp, 'Path', prefix )
+    simple_element( fp, 'FullName', prefix+'/'+name )
+    if cmdline:
+        simple_element( fp, 'FullCommandLine', cmdline )
+
+    start_element( fp, 'Results' )
+
+    if runtime != None:
+        write_float_measurement( fp, 'Execution Time', runtime )
+
+    if detail:
+        write_string_measurement( fp, 'Completion Status', detail )
+
+    if output:
+        write_measurement( fp, output )
+
+    end_element( fp, 'Results' )
+    end_element( fp, 'Test' )
+
+
+def write_float_measurement( fp, name, value ):
+    ""
+    start_element( fp, 'NamedMeasurement', type="numeric/double", name=name )
+    simple_element( fp, 'Value', str(value) )
+    end_element( fp, 'NamedMeasurement' )
+
+
+def write_string_measurement( fp, name, value ):
+    ""
+    start_element( fp, 'NamedMeasurement', type="text/string", name=name )
+    simple_element( fp, 'Value', str(value) )
+    end_element( fp, 'NamedMeasurement' )
+
+
+def write_measurement( fp, value ):
+    ""
+    start_element( fp, 'Measurement' )
+    simple_element( fp, 'Value', str(value) )
+    end_element( fp, 'Measurement' )
+
+
+def start_element( fp, elmt_name, **kwargs ):
+    ""
+    fp.write( '<'+elmt_name )
+    for n,v in kwargs.items():
+        fp.write( ' '+n+'="'+v+'"' )
+    fp.write( '>\n' )
+
+
+def end_element( fp, elmt_name ):
+    ""
+    fp.write( '</'+elmt_name+'>\n' )
 
 
 def write_xml_header( fp ):
